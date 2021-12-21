fa49c96589c5067399987e6f3a5da1ab87a7631c
Atilla Aydemir <atilla-734@hotmail.com>
2021-12-22 02:10:56 +0300

    added scroll and other component.

--- app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
@@ -5,20 +5,23 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ayd.foodrecipe.ui.theme.FoodRecipeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +41,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainMethod() {
 
+    val scrollState = rememberScrollState()
+
+    // Smooth scroll to specified pixels on first composition
+    LaunchedEffect(Unit) { scrollState.animateScrollTo(10000) }
+
     val context = LocalContext.current // for toastMessage
 
     Scaffold(
@@ -51,6 +59,7 @@ fun MainMethod() {
 
             Column(horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
+                               .verticalScroll(scrollState) //for scroll
             ) {
                Image(painter = painterResource(id =R.drawable.pannacotta), contentDescription ="")
 
@@ -81,6 +90,34 @@ fun MainMethod() {
                     }
 
                 }
+            
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 10.dp)) {
+                    Text(text = "Panna Cotta",
+                        color = colorResource(id = R.color.mainColor3),
+                        fontSize = 30.sp)
+                    
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Texts(content = "Milk dessert")
+                        Texts(content = "22/12/2021")
+                    }
+                }
+
+                Text(text = "Step 1\n" +
+                        "Pour milk into a small bowl, and stir in the gelatin powder. Set aside.\n" +
+                        "\n" +
+                        "Step 2\n" +
+                        "In a saucepan, stir together the heavy cream and sugar, and set over medium heat. Bring to a full boil, watching carefully, as the cream will quickly rise to the top of the pan. Pour the gelatin and milk into the cream, stirring until completely dissolved. Cook for one minute, stirring constantly. Remove from heat, stir in the vanilla and pour into six individual ramekin dishes.\n" +
+                        "\n" +
+                        "Step 3\n" +
+                        "Cool the ramekins uncovered at room temperature. When cool, cover with plastic wrap, and refrigerate for at least 4 hours, but preferably overnight before serving.",
+                    modifier = Modifier.padding(all = 10.dp),
+                    textAlign = TextAlign.Center)
+            
             }
         }
     )
