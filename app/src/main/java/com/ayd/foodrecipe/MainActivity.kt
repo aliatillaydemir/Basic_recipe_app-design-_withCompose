40a495b8e332494fde223f0f0be3d05b43f21bfa
Atilla Aydemir <atilla-734@hotmail.com>
2021-12-22 01:43:29 +0300

    added like and comments button.

--- app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.ayd.foodrecipe
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +38,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainMethod() {
 
+    val context = LocalContext.current // for toastMessage
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -46,6 +54,33 @@ fun MainMethod() {
             ) {
                Image(painter = painterResource(id =R.drawable.pannacotta), contentDescription ="")
 
+                Row(modifier = Modifier.fillMaxWidth()) {
+
+                    Button(  //button1
+                      onClick = { Toast.makeText(context, "u like this!", Toast.LENGTH_SHORT).show()},
+
+                            colors = ButtonDefaults.buttonColors(
+                            backgroundColor = colorResource(id = R.color.mainColor)
+                        ),
+                      modifier = Modifier.weight(1f)
+                      
+                  ) {
+                      Texts(content = "Like")
+                  }
+
+                    Button(   //button2
+                        onClick = { Toast.makeText(context, "Comment!", Toast.LENGTH_SHORT).show()},
+
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = colorResource(id = R.color.mainColor3)
+                        ),
+                        modifier = Modifier.weight(1f)
+
+                    ) {
+                        Texts(content = "Comment")
+                    }
+
+                }
             }
         }
     )
