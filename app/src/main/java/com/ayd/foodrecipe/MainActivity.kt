7fdd7360bade6f20279b0ade08620d7d29213b0d
Ali Atilla Aydemir <atilla734@gmail.com>
2022-10-28 01:11:17 +0300

    commit&push with git fork to step3Fork

--- app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/ayd/foodrecipe/MainActivity.kt
@@ -51,7 +51,7 @@ fun MainMethod() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {Text(text = "Recipe App")},
+                title = {Text(text = "Recipe Apppping Yeaaa")},
                 backgroundColor = colorResource(id = R.color.mainColor)
             )
 
